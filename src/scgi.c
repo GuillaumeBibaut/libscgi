786e553ed2cbd94b04d913640afc9c6c27732d34
Guillaume Bibaut <yom@iaelu.net>
2013-07-17 12:20:11 +0200

    do not flush everytime something is printed

--- src/scgi.c
+++ src/scgi.c
@@ -148,7 +148,9 @@ void scgi_headers_print(t_scgi *ctx) {
 void scgi_printf(t_scgi *ctx, const char *fmt, ...) {
     va_list ap;
     char *str;
+    bool flush;
 
+    flush = false;
     if (ctx->buffered && !ctx->forceflush) {
         va_start(ap, fmt);
         if (vasprintf(&str, fmt, ap) == -1) {
@@ -177,18 +179,23 @@ void scgi_printf(t_scgi *ctx, const char *fmt, ...) {
 
         if (!ctx->buffer.flushed && ctx->buffer.length != 0) {
             scgi_buffer_flush(&(ctx->buffer), ctx->_outstream, false);
+            flush = true;
         }
 
         va_start(ap, fmt);
         (void)vfprintf(ctx->_outstream, fmt, ap);
         va_end(ap);
-        fflush(ctx->_outstream);
+        if (flush) {
+            fflush(ctx->_outstream);
+        }
     }
 }
 
 
 void scgi_puts(t_scgi *ctx, const char *str) {
+    bool flush;
 
+    flush = false;
     if (ctx->buffered && !ctx->forceflush) {
         if (scgi_buffer_write(&(ctx->buffer), str) != 0) {
             if (ctx->exit) {
@@ -202,10 +209,13 @@ void scgi_puts(t_scgi *ctx, const char *str) {
 
         if (!ctx->buffer.flushed && ctx->buffer.length != 0) {
             scgi_buffer_flush(&(ctx->buffer), ctx->_outstream, false);
+            flush = true;
         }
 
         fputs(str, ctx->_outstream);
-        fflush(ctx->_outstream);
+        if (flush) {
+            fflush(ctx->_outstream);
+        }
     }
 }
 
